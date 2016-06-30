6b13369061babee5e44f4e9809169c69e89da3b2
Rafael Kerr <rafael.kerr@gmail.com>
2016-06-30 19:32:18 -0300

    add main func

--- hello.go
+++ hello.go
@@ -22,5 +22,52 @@ type Numverify struct {
 
 
 func main() {
-  fmt.Printf("Hello, Word!")
+	phone := "14158586273"
+	// QueryEscape escapes the phone string so
+	// it can be safely placed inside a URL query
+	safePhone := url.QueryEscape(phone)
+
+	url := fmt.Sprintf("http://apilayer.net/api/validate?access_key=YOUR_ACCESS_KEY&number=%s", safePhone)
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+	// For control over HTTP client headers,
+	// redirect policy, and other settings,
+	// create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	// Do sends an HTTP request and
+	// returns an HTTP response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Fill the record with the data from the JSON
+	var record Numverify
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Phone No. = ", record.InternationalFormat)
+	fmt.Println("Country   = ", record.CountryName)
+	fmt.Println("Location  = ", record.Location)
+	fmt.Println("Carrier   = ", record.Carrier)
+	fmt.Println("LineType  = ", record.LineType)
+
 }
