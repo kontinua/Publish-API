33793006660ca3894dc2be148a7a4e001550eaed
Rafael Kerr <rafael.kerr@gmail.com>
2016-06-30 19:31:01 -0300

    add imports to the main project

--- hello.go
+++ hello.go
@@ -1,5 +1,25 @@
 package main
-import "fmt"
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Numverify struct {
+	Valid               bool   `json:"valid"`
+	Number              string `json:"number"`
+	LocalFormat         string `json:"local_format"`
+	InternationalFormat string `json:"international_format"`
+	CountryCode         string `json:"country_code"`
+	CountryName         string `json:"country_name"`
+	Location            string `json:"location"`
+	Carrier             string `json:"carrier"`
+	LineType            string `json:"line_type"`
+}
+
 
 func main() {
   fmt.Printf("Hello, Word!")
