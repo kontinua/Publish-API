bccb3f6c43477a99336649e14137e2319afbe562
Rafael Kerr <rafael.kerr@gmail.com>
2016-07-02 22:56:41 -0300

    Update users.go
    add func PrintHugeParams

--- users.go
+++ users.go
@@ -30,3 +30,32 @@ func init() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello from the Go app")
 }
+
+
+func PrintHugeParams(fset *token.FileSet, info *types.Info, files []*ast.File) {
+	checkTuple := func(descr string, tuple *types.Tuple) {
+		for i := 0; i < tuple.Len(); i++ {
+			v := tuple.At(i)
+			if sz := sizeof(v.Type()); sz > int64(*bytesFlag) {
+				fmt.Printf("%s: %q %s: %s = %d bytes\n",
+					fset.Position(v.Pos()),
+					v.Name(), descr, v.Type(), sz)
+			}
+		}
+	}
+	checkSig := func(sig *types.Signature) {
+		checkTuple("parameter", sig.Params())
+		checkTuple("result", sig.Results())
+	}
+	for _, file := range files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			switch n := n.(type) {
+			case *ast.FuncDecl:
+				checkSig(info.Defs[n.Name].Type().(*types.Signature))
+			case *ast.FuncLit:
+				checkSig(info.Types[n.Type].Type.(*types.Signature))
+			}
+			return true
+		})
+	}
+}
